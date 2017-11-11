dadcd540b900cc07711cf67bcbc359a651c16e1d
Joël Gaujard <j.gaujard@gmail.com>
2017-11-12 00:39:01 +0100

    Start to customize template

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is M.Reza", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Joël G.", "I'm a Web Developer", "Love Algorithm."],
         typeSpeed: 100,
         backDelay: 900,
         // loop
